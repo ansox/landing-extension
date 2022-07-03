ca0b4dbe91385901a7742a72ed54f687b0ac3500
Anso <anderson.floriano@gmail.com>
2022-07-03 11:31:27 -0300

    Add link to app

--- pages/end.tsx
+++ pages/end.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import logo from '../public/logo_extension.svg';
 import NextLink from 'next/link'
 import btnBack from '../public/bt_voltar.svg';
+import Link from 'next/link';
 
 const End: NextPage = () => {
   return (
@@ -17,9 +18,11 @@ const End: NextPage = () => {
       <div className='w-full mt-14 flex flex-col justify-center item-center gap-10'>
         <Image src={logo} alt="logo extension" />
 
-        <div className='bg-[#ffcc00] mx-6 p-6 rounded-xl uppercase'>
-          <p className='text-center font-sans text-lg '>Baixar a nova versão do extension solution</p>
-        </div>
+          <Link href="https://extension4.s3.sa-east-1.amazonaws.com/extension4.apk">
+            <div className='bg-[#ffcc00] mx-6 p-6 rounded-xl uppercase'>
+              <a className='text-center font-sans text-lg '>Baixar a nova versão do extension solution</a>
+            </div>
+          </Link>
       </div>
       
     </>
